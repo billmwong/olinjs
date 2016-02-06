aebfe2e9407231d7cb5c7df753799eb4af6fe1fe
jagreene <austin.greene@students.olin.edu>
2016-02-06 18:09:08 -0500

    GRADED

--- lessons/03-express-templates/myHomework/routes/index.js
+++ lessons/03-express-templates/myHomework/routes/index.js
@@ -5,6 +5,8 @@ var db = require('../fakeDatabase');
 possibleNames = ['Alfie','Angel','Argus','Arwen','Ashton','Baby','Bailey','Bassil','Beage','Bender','Benny','Big Red','Big Sammy','Binka','Binx','Blackie','Bobo','Boogie','Boris','Bowser','Brandy','Buddy','Butterscotch','Cally','Caramel','Casey','Catherine','Cece','Celine','Cherio','Chloe','Churchill','Cinders','Clarence','Claude','Cleo','Coffee','Cookie','Cooper','Cosmo','Couch Potato','Daisy','Dakota','Dark Moon','David','Denver','Diesel','Dilon','Disney','Dissy','Divine','Dude','Ebony','Enzo','Faggie','Fagison','Felix','Ferris','Fifi','Flowerbee','Fluffernet','Fluffy','Fosters','Freckles','Frollo','Freckuls','Fudge','Fudge','Fuffie','Gabrielle','Gadget','Gary','Ginger','Gipsy','Gizmo','Gore','Grace','Griswald','Grizz','Grizzabella','Harley','Hazel','Henry','Hex','Hoppy','Indy','Izzy','Jasmine','Jay','Jinx','Jon','Jymes Dean','Katie','Kelee','Kenny','Kirby','Kitty','Kliff','Kool Kat','Lady','Larry','Lera','Lexie','Lincoln','Little','Lucy','Mac','Mackie','Marbles','Mario','Marley','Marmelade','Martin','Max','Maxine','Merlin','Micky','Midnight','Mippen','Misty','Missy','Mittens','Mitzy','Molly','Moon Pie','Moriarty','Moritz','Moses','Mozart','Ms Kitty','Myah','Nala','Natasha','Nemo','Niglet','Nikita','Noodles','Noodlehead','Oliver','Oreo','Orwell','Oscar','Panda','Patsy','Paws','Peanut','Peter','Phoebe','Pinkle','Plinky','Poocat','Pooder','Pookie','Prince','Princess','Pumpkin','Punkin','Purr','Rajha','Ralph','Rascal','Romeo','Roxie','Rylie','Sadie','Salem','Sammy','Sasha','Scooter','Scout','Secret','Shadow','Shane','Shelby','Simba','Simon','Simpson','Slippers','Smudge','Sophie','Soushi','Sparkle','Sputnick','Stanley','Starlett','Sticky','Stimpy','Stray','Sugar','Sunny','Tater','Tigger','Tinkerbell','Tina','Tiny','Tobi','Tori','Tricia','Tucker','Tuna','Twiggy','Walter','Whiskers','Willow','Wyatt','Xman','Zakkie','Zebra'];
 possibleColors = ['black','blue','brown','gray','green','orange','pink','purple','red','white','yellow'];
 
+
+//might as well use the object oriented syntax
 function Cat(name, age, colors) {
 	var cat = {
 		name: name,
@@ -36,6 +38,9 @@ router.get('/cats/new', function(req, res, next) {
 	var colors = [];
 	colors.push(possibleColors[Math.floor(Math.random()*possibleColors.length)]);
 	colors.push(possibleColors[Math.floor(Math.random()*possibleColors.length)]);
+	//fun fact you could just use:
+	//	db.add({name, age, colors})
+	//if you don't put in a key it auto makes one with the same name as the variable you put in. snazzy af (thanks to the new es6 synax that node supports). Be careful using this on the client though cause legacy browsers won't support it
 	db.add(Cat(name, age, colors));
 	res.render('newcat', {name: name});
 });
