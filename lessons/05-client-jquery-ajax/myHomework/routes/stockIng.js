3e25dd64d1bc774723667ff6f7dae749f10995c1
Bill Wong <billmwong@gmail.com>
2016-02-12 16:00:15 -0500

    cleanup

--- lessons/05-client-jquery-ajax/myHomework/routes/stockIng.js
+++ lessons/05-client-jquery-ajax/myHomework/routes/stockIng.js
@@ -3,14 +3,6 @@ var Ingredient = require('../models/ingredientModel.js');
 var routes = {};
 
 routes.stockIngPOST = function(req, res) {
-	// Ingredient.find({id: req.body.id}, function(err, Ing) {
-	// })
-	// Ingredient.findByIdAndRemove(req.body.id, function() {
-	// 	console.log('removed id '+req.body.id+' from the database');
-	// });
-	// thisIng.save();
-	// console.log(JSON.stringify(thisIng));
-	// res.send(thisIng);
 	Ingredient.findById(req.body.id, function (err, ing) {
 		if (err) return handleError(err);
 
