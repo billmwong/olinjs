0f9b831d105aa1e62fc6810c775e18d5b6d8f509
Bill Wong <billmwong@gmail.com>
2016-02-15 00:34:12 -0500

    finished with formsApp

--- lessons/05-client-jquery-ajax/formsApp/routes/getCat.js
+++ lessons/05-client-jquery-ajax/formsApp/routes/getCat.js
@@ -2,27 +2,31 @@ var path = require('path');
 
 var routes = {};
 
-var getCatImage = function(catParams) {
+var getCatImage = function(catParams, absolute) {
   var imageLocation;
+  var happyCat = "images/cat.jpg";
+  var grumpyCat = "images/grumpy.jpeg";
   switch (catParams.mood) {
     case "happy":
-      imageLocation = path.join(__dirname,"../public/images/cat.jpg");
+      imageLocation = absolute ? path.join(__dirname,"../public/", happyCat) : happyCat;
       break;
     case "grumpy":
-      imageLocation = path.join(__dirname, "../public/images/grumpy.jpeg");
+      imageLocation = absolute ? path.join(__dirname, "../public/", grumpyCat) : grumpyCat;
       break;
   }
   return imageLocation;
 }
 
 routes.getCatGET = function(req, res) {
-  console.log(req.query);
-  res.sendFile(getCatImage(req.query));
+  if (req.xhr) {
+    res.send(getCatImage(req.query));
+  } else {
+    res.sendFile(getCatImage(req.query, true));
+  }
 };
 
 routes.getCatPOST = function(req, res) {
-  console.log(req.body);
-  res.sendFile(getCatImage(req.body));
+  res.sendFile(getCatImage(req.body, true));
 };
 
 module.exports = routes;
