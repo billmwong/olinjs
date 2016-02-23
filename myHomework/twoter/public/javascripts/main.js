db4aba14c1c384283c535e2bf3c51fd63f8dd448
Bill Wong <billmwong@gmail.com>
2016-02-23 01:42:50 -0500

    highlights user's twotes when clicked on

--- myHomework/twoter/public/javascripts/main.js
+++ myHomework/twoter/public/javascripts/main.js
@@ -37,3 +37,80 @@ $twoteWall.on('click', '.delete-twote-btn', function() {
 		.done(onSuccessDeleteTwote)
 		.error(onError);
 })
+
+$('.user-btn').click(function() {
+	var userID = $(this).attr('id');
+	var $thisUsersTwotes = $('div.well[data-creator-id="'+userID+'"]');
+	if ($(this).siblings().prop('checked')) {
+		// Revert back to previous color
+		$thisUsersTwotes.css("background-color", "#f5f5f5")
+	} else {
+		// Highlight this user's twotes
+		$thisUsersTwotes.css("background-color", "#d9edf7")
+	}
+});
+
+// TAKEN FROM http://bootsnipp.com/snippets/featured/jquery-checkbox-buttons
+$('.button-checkbox').each(function () {
+
+    // Settings
+    var $widget = $(this),
+        $button = $widget.find('button'),
+        $checkbox = $widget.find('input:checkbox'),
+        color = $button.data('color'),
+        settings = {
+            on: {
+                icon: 'glyphicon glyphicon-check'
+            },
+            off: {
+                icon: 'glyphicon glyphicon-unchecked'
+            }
+        };
+
+    // Event Handlers
+    $button.on('click', function () {
+        $checkbox.prop('checked', !$checkbox.is(':checked'));
+        $checkbox.triggerHandler('change');
+        updateDisplay();
+    });
+    $checkbox.on('change', function () {
+        updateDisplay();
+    });
+
+    // Actions
+    function updateDisplay() {
+        var isChecked = $checkbox.is(':checked');
+
+        // Set the button's state
+        $button.data('state', (isChecked) ? "on" : "off");
+
+        // Set the button's icon
+        $button.find('.state-icon')
+            .removeClass()
+            .addClass('state-icon ' + settings[$button.data('state')].icon);
+
+        // Update the button's color
+        if (isChecked) {
+            $button
+                .removeClass('btn-default')
+                .addClass('btn-' + color + ' active');
+        }
+        else {
+            $button
+                .removeClass('btn-' + color + ' active')
+                .addClass('btn-default');
+        }
+    }
+
+    // Initialization
+    function init() {
+
+        updateDisplay();
+
+        // Inject the icon if applicable
+        if ($button.find('.state-icon').length == 0) {
+            $button.prepend('<i class="state-icon ' + settings[$button.data('state')].icon + '"></i> ');
+        }
+    }
+    init();
+});
